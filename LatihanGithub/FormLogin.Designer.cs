9650479f890a368ac57a4a6a841e7dfac82e0eeb
ferdiya27 <ferpribpridi@gmail.com>
2025-10-06 11:08:04 +0800

    update form login

--- LatihanGithub/FormLogin.Designer.cs
+++ LatihanGithub/FormLogin.Designer.cs
@@ -37,7 +37,6 @@
             ClientSize = new Size(800, 450);
             Name = "FormLogin";
             Text = "FormLogin";
-            Load += this.FormLogin_Load;
             ResumeLayout(false);
         }
 
