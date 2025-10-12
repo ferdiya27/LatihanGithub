0344304cff527260ab09a247858ff05747bded71
shalsabilaalghaida <shalsabilaalghaida4@gmail.com>
2025-10-12 19:19:12 +0800

    tes

--- LatihanGithub/FormSiswa.Designer.cs
+++ LatihanGithub/FormSiswa.Designer.cs
@@ -34,7 +34,7 @@
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(800, 643);
             Name = "FormSiswa";
             Text = "FormSiswa";
             ResumeLayout(false);
