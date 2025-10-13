dd7132a8d7bb1c8059f2a0ba2861c3669d68c8bd
ferdiya27 <ferpribpridi@gmail.com>
2025-10-13 09:37:13 +0800

    new login

--- LatihanGithub/FormLogin.Designer.cs
+++ LatihanGithub/FormLogin.Designer.cs
@@ -28,18 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
+            txtUser = new TextBox();
+            txtPass = new TextBox();
+            lblUser = new Label();
+            lblPass = new Label();
+            btnLogin = new Button();
+            btnExit = new Button();
             SuspendLayout();
+            // 
+            // txtUser
+            // 
+            txtUser.Location = new Point(230, 86);
+            txtUser.Name = "txtUser";
+            txtUser.Size = new Size(457, 31);
+            txtUser.TabIndex = 0;
+            // 
+            // txtPass
+            // 
+            txtPass.Location = new Point(230, 180);
+            txtPass.Name = "txtPass";
+            txtPass.Size = new Size(457, 31);
+            txtPass.TabIndex = 1;
+            // 
+            // lblUser
+            // 
+            lblUser.AutoSize = true;
+            lblUser.Location = new Point(129, 86);
+            lblUser.Name = "lblUser";
+            lblUser.Size = new Size(91, 25);
+            lblUser.TabIndex = 2;
+            lblUser.Text = "Username";
+            // 
+            // lblPass
+            // 
+            lblPass.AutoSize = true;
+            lblPass.Location = new Point(129, 180);
+            lblPass.Name = "lblPass";
+            lblPass.Size = new Size(87, 25);
+            lblPass.TabIndex = 3;
+            lblPass.Text = "Password";
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(230, 246);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(112, 34);
+            btnLogin.TabIndex = 4;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(554, 246);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(112, 34);
+            btnExit.TabIndex = 5;
+            btnExit.Text = "Exit";
+            btnExit.UseVisualStyleBackColor = true;
             // 
             // FormLogin
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.CadetBlue;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnExit);
+            Controls.Add(btnLogin);
+            Controls.Add(lblPass);
+            Controls.Add(lblUser);
+            Controls.Add(txtPass);
+            Controls.Add(txtUser);
             Name = "FormLogin";
             Text = "FormLogin";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtUser;
+        private TextBox txtPass;
+        private Label lblUser;
+        private Label lblPass;
+        private Button btnLogin;
+        private Button btnExit;
     }
 }
--- LatihanGithub/FormLogin.cs
+++ LatihanGithub/FormLogin.cs
@@ -6,5 +6,35 @@ namespace LatihanGithub
         {
             InitializeComponent();
         }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtUser.Text == "")
+            {
+                MessageBox.Show("Username tidak boleh kosong!!");
+                txtUser.Focus();
+                return;
+            }
+            if (txtPass.Text == "")
+            {
+                MessageBox.Show("Password tidak boleh kosong");
+                txtPass.Focus();
+                return;
+            }
+            if (txtUser.Text == "Admin" && txtPass.Text == "1234")
+            {
+                MessageBox.Show("Login Berhasil!!");
+                FormMenu fd = new FormMenu();
+                fd.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Login Gagal!!");
+                txtUser.Text = "";
+                txtPass.Clear();
+                txtUser.Clear();
+            }
+        }
     }
-}
+    }
+
